47b87990a9584f2de7d786f6b4dce4635946511e
Nikita <nikitapol0818@gmail.com>
2025-04-25 23:26:24 +0300

    .

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
-# main.py
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ConversationHandler
-from handlers import start, handle_button_click, handle_text
-from states import ADD_TASK, CHOOSE_DATE, ENTER_TIME, ENTER_COMMENT, LIST_TASKS
-from keyboard import start_keyboard, back_button
-import logging
+from handlers import start, handle_button_click, handle_text, ADD_TASK, ENTER_TIME, ENTER_COMMENT
 
-# Настройки
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+    states={
+        ADD_TASK: [MessageHandler(filters.TEXT, handle_text)],
+        ENTER_TIME: [MessageHandler(filters.TEXT, handle_text)],
+        ENTER_COMMENT: [MessageHandler(filters.TEXT, handle_text)]
+    },
+    fallbacks=[CallbackQueryHandler(handle_button_click)]
+)
 
-def main():
-    # Создаем экземпляр приложения
+async def main():
     application = Application.builder().token('7447545827:AAFf6HxnyeZRhbEGAPpMsS5jDwjzh-AO81o').build()
-
-    # Главный разговор
-    conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(start, pattern='^start$')],
-        states={
-            ADD_TASK: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text)],
-            CHOOSE_DATE: [CallbackQueryHandler(handle_button_click, pattern='^date_')],
-            ENTER_TIME: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text)],
-            ENTER_COMMENT: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text)],
-            LIST_TASKS: [CallbackQueryHandler(handle_button_click, pattern='^task_')]
-        },
-        fallbacks=[],
-    )
-
-    # Добавляем обработчики
     application.add_handler(conv_handler)
-    application.add_handler(CallbackQueryHandler(handle_button_click))
+    await application.run_polling()
 
-    # Запускаем бота (asynchronous)
-    application.run_polling()
-
-# Если код запускается напрямую
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
