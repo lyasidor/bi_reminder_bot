94e512d87fbb2563d78e4edb1c50afbbe651bf2a
Nikita <nikitapol0818@gmail.com>
2025-04-25 21:11:08 +0300

    .

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_TOKEN = "7777339725:AAHd7AkbFBYRrcUl4wwGdbDsKvBo0b0FMGk"
+BOT_TOKEN = "7777339725:AAHd7AkbFBYRrcUl4wwGdbDsKvBo0b0FMGk"
 DATABASE_URI = "sqlite:///mydatabase.db"
 TIMEZONE = "Europe/Moscow"
